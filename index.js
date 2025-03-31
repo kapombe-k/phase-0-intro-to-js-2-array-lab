3748e279845c7d347cfdb51bbea610da74d8ac99
Eugene Kapombe <kapombek@gmail.com>
2025-03-31 15:50:34 +0300

    added array methods

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+destructivelyAppendCat = (name) => {
+    cats.push(name);
+};
+
+destructivelyPrependCat = (name) => {
+    cats.unshift(name);
+};
+
+destructivelyRemoveLastCat = () => {
+    cats.pop();
+};
+ 
+destructivelyRemoveFirstCat = () => {
+    cats.shift();
+};
+ 
+appendCat = (name) => {
+    return [...cats, name];
+};
+ 
+prependCat = (name) => {
+    return [name,...cats]
+};
+ 
+removeLastCat = (name) => {
+    return cats.slice(0, cats.length - 1);
+};
+ 
+removeFirstCat = (name) => {
+    return cats.slice(1);
+};
+ 
